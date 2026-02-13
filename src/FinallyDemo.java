c83ee1e8137855858d153a018ace1d4954c4e496
Bharath M Guptha <bharathmgupta@gmail.com>
2026-02-13 09:37:58 +0530

    Update FinallyDemo.java

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
-public class FinallyDemo {
-    
-        // TODO: Read a and b
-        
-        // TODO: try block
-        // Perform division
-        // Print "Result: " + result
-        
-        // TODO: catch ArithmeticException e
-        // Print "Error: " + e.getMessage()
-        
-        // TODO: finally block
-        // Print "Finally block executed"
+public class finallyBlock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter numerator and denominator :");
+        int numerator = sc.nextInt();
+        int denominator = sc.nextInt();
+
+        try {
+            int result = numerator/denominator;
+            System.out.println("result = "+result);
+        } catch (Exception e) {
+            System.out.println("error :"+e.getMessage());
+        } finally{
+            System.out.println("finally block executed");
+        }
     }
 }
